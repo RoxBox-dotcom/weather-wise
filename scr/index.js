2312fd14ee72daea1aa610d0fbb1eebe1802f742
Roxy <151556798+RoxBox-dotcom@users.noreply.github.com>
2023-12-02 17:34:37 +0100

    Added a function to change underlay according to temp

--- scr/index.js
+++ scr/index.js
@@ -21,6 +21,8 @@ function displayTemp(response) {
   windElement.innerHTML = response.data.wind.speed;
   humidityElement.innerHTML = response.data.temperature.humidity;
   dateElement.innerHTML = formatDate(date);
+
+  updateTemperatureCircle(temp);
 }
 
 function formatDate(date) {
@@ -56,3 +58,18 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Johannesburg");
+
+function updateTemperatureCircle(temperature) {
+  let circle = document.querySelector("#current-temperature-container");
+
+  if (temperature > 22) {
+    circle.style.background =
+      "linear-gradient(109.6deg, rgb(162, 2, 63) 11.2%, rgb(231, 62, 68) 53.6%, rgb(255, 129, 79) 91.1%)";
+  } else if (temperature >= 12) {
+    circle.style.background =
+      "linear-gradient(109.6deg, rgb(255, 207, 84) 11.2%, rgb(255, 158, 27) 91.1%)";
+  } else {
+    circle.style.background =
+      "radial-gradient(circle at 12.3% 19.3%, rgb(85, 88, 218) 0%, rgb(95, 209, 249) 100.2%)";
+  }
+}
